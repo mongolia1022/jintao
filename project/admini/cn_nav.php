d623033592a2bbc1cffb8d7e1f01d685a919a282
yuchao <305694029@qq.com>
2017-06-06 20:44:35 +0800

    开发

---
+++ project/admini/cn_nav.php
@@ -0,0 +1,30 @@
+<li><a href='./index.php?p=1'>企业展示</a><ul><li><a href='./index.php?p=29'>企业文化</a></li>
+<li><a href='./index.php?p=30'>企业荣誉</a></li>
+<li><a href='./index.php?p=31'>企业目标</a></li>
+</ul></li>
+<li><a href='./index.php?p=2'>新闻中心</a></li>
+<li><a href='./index.php?p=3'>产品展示</a><ul><li><a href='./index.php?p=9'>小程序</a><ul><li><a href='./index.php?p=32'>小分类1</a></li>
+</ul></li>
+<li><a href='./index.php?p=10'>公众号</a></li>
+<li><a href='./index.php?p=11'>网站</a></li>
+<li><a href='./index.php?p=12'>App</a></li>
+<li><a href='./index.php?p=13'>营销策划</a></li>
+</ul></li>
+<li><a href='./index.php?p=4'>生产中心</a></li>
+<li><a href='./index.php?p=5'>工程案例</a></li>
+<li><a href='./index.php?p=6'>联系我们</a></li>
+<li><a href='./index.php?p=7'>首页设置</a><ul><li><a href='./index.php?p=14'>产品推荐</a><ul><li><a href='./index.php?p=15'>推拉窗系列</a></li>
+<li><a href='./index.php?p=16'>平开窗系列</a></li>
+<li><a href='./index.php?p=17'>平开门系列</a></li>
+<li><a href='./index.php?p=18'>断桥隔热系列</a></li>
+<li><a href='./index.php?p=19'>幕墙系列</a></li>
+<li><a href='./index.php?p=20'>通用型材系列</a></li>
+</ul></li>
+<li><a href='./index.php?p=21'>推荐工程案例</a></li>
+<li><a href='./index.php?p=22'>关于我们文案</a></li>
+<li><a href='./index.php?p=23'>问答</a></li>
+<li><a href='./index.php?p=24'>理念</a></li>
+<li><a href='./index.php?p=25'>底部地址</a></li>
+<li><a href='./index.php?p=26'>底部版权</a></li>
+<li><a href='./index.php?p=27'>服务热线</a></li>
+</ul></li>
---
+++ project/admini/en_nav.php
@@ -0,0 +1,14 @@
+<li><a href='./index.php?p=1'>Products</a><ul><li><a href='./index.php?p=12'>product list1</a></li>
+<li><a href='./index.php?p=13'>product list2</a></li>
+<li><a href='./index.php?p=14'>product list3</a></li>
+</ul></li>
+<li><a href='./index.php?p=2'>Get inspired</a></li>
+<li><a href='./index.php?p=3'>In action</a></li>
+<li><a href='./index.php?p=4'>Service</a></li>
+<li><a href='./index.php?p=5'>Peace of mind</a></li>
+<li><a href='./index.php?p=6'>About us</a></li>
+<li><a href='./index.php?p=7'>Contact us</a></li>
+<li><a href='./index.php?p=8'>首页内容设置</a><ul><li><a href='./index.php?p=9'>关于我们</a></li>
+<li><a href='./index.php?p=10'>产品推荐</a></li>
+<li><a href='./index.php?p=11'>法律声明</a></li>
+</ul></li>
